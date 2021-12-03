4dbd4a1ef9833488c25876cdd9750730cbd28613
Idris Bankole <bankoleidris@gmail.com>
2021-12-03 20:21:58 +0100

    home

--- screens/Home.js
+++ screens/Home.js
@@ -11,10 +11,14 @@ import {
 import { COLORS, icons, images, FONTS } from "../constants";
 
 export default function Home() {
+  const renderHeader = () => {
+    return <View style={styles.header}></View>;
+  };
+
   return (
-    <View>
+    <SafeAreaView style={styles.container}>
       <Text>Home View here</Text>
-    </View>
+    </SafeAreaView>
   );
 }
 
@@ -32,5 +36,9 @@ const styles = StyleSheet.create({
     shadowOpacity: 0.1,
     shadowRadius: 3,
     elevation: 1,
+  },
+  header: {
+    flexDirection: "row",
+    height: 50,
   },
 });
