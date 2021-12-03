9118f6a19f8c5b40b3e83ea2cc04c5616a82229b
Idris Bankole <bankoleidris@gmail.com>
2021-12-03 16:57:45 +0100

    screens ++ navigators

---
+++ navigation/tabs.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+
+export default function tabs() {
+  return (
+    <View>
+      <Text></Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({});
