3a67881442e1c9d515072116cf6abfad5bf1572f
Idris Bankole <bankoleidris@gmail.com>
2021-12-04 18:35:59 +0100

    counter

--- screens/Restaurant.js
+++ screens/Restaurant.js
@@ -144,6 +144,8 @@ export default function Restaurant({ route, navigation }) {
                   </TouchableOpacity>
                 </View>
               </View>
+
+              {/* NAME AND DESCRIPTION */}
             </View>
           </>
         ))}
