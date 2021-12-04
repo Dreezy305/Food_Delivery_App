e9003681ad9ed46f4272cfc05afbcfcdad419241
Idris Bankole <bankoleidris@gmail.com>
2021-12-04 19:45:57 +0100

    counter

--- screens/Restaurant.js
+++ screens/Restaurant.js
@@ -83,6 +83,10 @@ export default function Restaurant({ route, navigation }) {
         scrollEventThrottle={16}
         snapToAlignment="center"
         showsHorizontalScrollIndicator={false}
+        onScroll={Animated.event(
+          [{ nativeEvent: { contentOffset: { x: scrollX } } }],
+          { useNativeDriver: false }
+        )}
       >
         {restaurant?.menu.map((item, index) => (
           <>
@@ -192,7 +196,57 @@ export default function Restaurant({ route, navigation }) {
     );
   };
 
-  const renderDots = () => {};
+  const renderDots = () => {
+    const dotsPosition = Animated.divide(scrollX, SIZES.width);
+
+    return (
+      <View style={{ height: 30 }}>
+        <View
+          style={{
+            flexDirection: "row",
+            alignItems: "center",
+            justifyContent: "center",
+            height: SIZES.padding,
+          }}
+        >
+          {restaurant?.menu.map((item, index) => {
+            const opacity = dotsPosition.interpolate({
+              inputRange: [index - 1, index, index + 1],
+              outputRange: [0.3, 1, 0.3],
+              extrapolate: "clamp",
+            });
+
+            const dotSize = dotsPosition.interpolate({
+              inputRange: [index - 1, index, index + 1],
+              outputRange: [SIZES.base * 0.8, 10, SIZES.base * 0.8],
+              extrapolate: "clamp",
+            });
+
+            const dotColor = dotsPosition.interpolate({
+              inputRange: [index - 1, index, index + 1],
+              outputRange: [COLORS.darkgray, COLORS.primary, COLORS.darkgray],
+              extrapolate: "clamp",
+            });
+            return (
+              <>
+                <Animated.View
+                  key={`dot-${index}`}
+                  opacity={opacity}
+                  style={{
+                    borderRadius: SIZES.radius,
+                    marginHorizontal: 6,
+                    width: dotSize,
+                    height: dotSize,
+                    backgroundColor: dotColor,
+                  }}
+                />
+              </>
+            );
+          })}
+        </View>
+      </View>
+    );
+  };
 
   const renderOrder = () => {
     return <View>{renderDots()}</View>;
