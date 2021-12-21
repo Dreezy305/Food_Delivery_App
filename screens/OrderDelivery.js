73b1bbfd85a56875edabdd836deed335dbd72413
Idris Bankole <bankoleidris@gmail.com>
2021-12-21 09:38:25 +0100

    order delivery

--- screens/OrderDelivery.js
+++ screens/OrderDelivery.js
@@ -1,12 +1,12 @@
 import React from "react";
+import { render } from "react-dom";
 import { StyleSheet, Text, View } from "react-native";
 
 export default function OrderDelivery() {
-  return (
-    <View>
-      <Text>OrderDelivery</Text>
-    </View>
-  );
+  const renderMap = () => {
+    return <View></View>;
+  };
+  return <View style={{ flex: 1 }}>{renderMap()}</View>;
 }
 
 const styles = StyleSheet.create({});
